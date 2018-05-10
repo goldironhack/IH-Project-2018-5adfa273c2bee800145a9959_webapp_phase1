e91c6a5d276f6e087e4a32e6ec7f87cf4cf7cc1f
ermahechap <ermahechap@unal.edu.co>
2018-05-09 23:36:48 -0400

    Automatic commit. Done before go to queltrics

--- README.md
+++ README.md
@@ -1,3 +1,32 @@
-## webapp_phase1
+# NYU Stern School of Business - Rooms
 
-undefined
+> Keywords: Mapas, Visualización, Localización
+
+**Descripción datasets:**
+*Nota: Los datasets estan siendo cargados, pero no todos estan siendo utilizados.*
+ - [Neighborhood Names GIS][https://catalog.data.gov/dataset/neighborhood-names-gis][Json] Posee informacion de los barrios de NY.
+ - [NY Districts geoshapes][https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson][GeoJson] Posee información de los Distritos de NY y sus formas (polígonos).
+ - [Crimes in NY][https://data.cityofnewyork.us/Public-Safety/NYPD-Complaint-Data-Historic/qgea-i56i/data][Json]Base de datos con reportes de crímenes en NY con lugar de ocurrencia(lat,long), entidad que lo reporta, hora de ocurrencia y una descripción  breve del suceso.
+ - [Dataset contains information on New York City housing by building data][https://catalog.data.gov/dataset/housing-new-york-units-by-building][Json] Información de viviendas asequibles. Dicha información incluye su ubicación.
+
+**Descripción breve:**
+El proyecto es un mashup que incluye un mapa para visualizar información y ordenarla. El mapa incluye una sección en la que se van a añadir gráficos de D3.js que muestren el desempeño del distrito seleccionado. Aquí se pueden seleccionar los barrios a comparar en siguiente sección.
+
+En la sección de comparación se pretende generar una vista para comparar varios barrios/viviendas seleccionadas en el mapa, y determinar que valores se quieren considerar para clasificarlas.
+
+En la sección de descargas se le permite al usuario generar un informe con los datos que el desee incluir y generar un csv.
+
+**Descripción:**
+-   Map View:
+    1.  [Y] El mapa esta centrado sobre la NYU Stern School of Business.
+    2.  [Y] [Boroughs]El mapa hasta el momento incluye una vista basica de los boroughs y los permite cargar independientemente.
+    3. [N] No se han realizado más funcionalidades con los datos disponibles, ya que para la primera entrega tomé la decisión de adelantar el front-end para que posteriormente fuera más fácil añadir elementos a la aplicación.
+-   Data Visualization:
+
+    1.  [N]Aún no hay uso de gráficos, sin embargo ya hay una parte dispuesta para ello.
+-   Interaction Form:
+
+    1.  [Y/N] [List] Hay un pequeño formulario que posee la funcionalidad de seleccionar el Borough deseado, aún así falta añadir funcionalidades al panel del mapa.
+
+6.  Test Case:
+Hasta el momento se ha probado en Firefox y Chrome, y el desempeño de la aplicación ha sido similar. También se ha probado la aplicación con las funcionalidades de estos navegadores para simular un ambiente móvil, y se ha demostrado que funciona responsivamente.
--- index.js
+++ index.js
@@ -1,6 +1,115 @@
+/* -- Load the datasets --*/
 
-(function () {
-    document.getElementById("btn").addEventListener("click", function () {
-        alert("Hi! :)");
+
+var URLS={"neighborhood_names" : "https://data.cityofnewyork.us/api/views/xyye-rtrs/rows.json?accessType=DOWNLOAD",
+    "geoshapes" : "https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson",
+    "crime" : "https://data.cityofnewyork.us/api/views/qgea-i56i/rows.json?accessType=DOWNLOAD",
+    "housing" : "https://data.cityofnewyork.us/api/views/hg8x-zxpr/rows.json?accessType=DOWNLOAD"
+};
+
+var boroughs = [
+    {name:"Manhattan",color: "#06e908"},//1
+    {name:"Bronx",color: "#134bdb"},//2
+    {name:"Brooklyn",color:"#d7480b"},//3
+    {name:"Queens",color: "#c211ee"},//4
+    {name:"Staten Island",color: "#dbd318"}//5
+];
+var datasets = {};
+
+function loadData(){
+    for(let key in URLS){
+        $.get(URLS[key],function(){})
+            .done(function(data){
+                switch (key) {
+                    case Object.keys(URLS)[0]:
+                        datasets[key] = data.data;
+                    break;
+                    case Object.keys(URLS)[1]:
+                        datasets[key] = data;
+                    break;
+                    case Object.keys(URLS)[2]:
+                        datasets[key] = data;
+                    break;
+                    case Object.keys(URLS)[3]:
+                        datasets[key] = data;
+                    break;
+                    default:
+                }
+            })
+            .fail(function(error){console.error(error);});
+    }
+}
+
+/*-- Google maps interaction scripts -- */
+
+var mapTop;
+var coordNYU = {lat: 40.729218, lng: -73.996492}
+
+function onGoogleMapResponse(){
+    mapTop = new google.maps.Map(document.getElementById('mapContainer'),{
+        center: coordNYU,
+        zoom: 10,
+        gestureHandling: 'greedy', // avoid ctrl + scroll
+        zoomControl: true,
+        zoomControlOptions:{
+            position: google.maps.ControlPosition.LEFT_BOTTOM
+        },
+        fullscreenControl: false,
+        mapTypeControl: false,
+        streetViewControl: false
     });
-})();
+    var fixedMarker = new google.maps.Marker({
+        position: coordNYU,
+        map: mapTop,
+        title: 'NYU!',
+    });
+    mapTop.data.loadGeoJson("https://services5.arcgis.com/GfwWNkhOj9bNBqoJ/arcgis/rest/services/nycd/FeatureServer/0/query?where=1=1&outFields=*&outSR=4326&f=geojson");
+    mapTop.data.setStyle({
+        visible : false
+    });
+}
+
+function showBoroughs(){
+    mapTop.data.setStyle(function(feature){
+        let boroCD = Math.floor(feature.getProperty("BoroCD")/100.0);
+        let color = boroughs[boroCD-1].color;
+        let vi =true;
+        if(boroCD != boroughChosen) vi = false;
+        return ({
+            strokeColor : color,
+            strokeWeight : 2,
+            visible : vi
+        });
+    });
+}
+
+
+/*-- Buttons and other interactions -- */
+$(document).ready(function(){
+    loadData();
+
+    /*Populate location lists*/
+    for(var i = 0 ; i<5;i++){
+        $('#location-map').append('<option value="' + (i+1) + '">' + boroughs[i].name + '</option>');
+    }
+});
+
+
+var boroughChosen = 0;
+$("#location-map").change(function(){
+    boroughChosen = this.value;
+    if(boroughChosen != 0){
+        $("#location-com-map").prop("disabled",false);
+        showBoroughs();
+    }else{
+        $("#location-com-map").empty();
+        $("#location-com-map").append('<option value="0">Seleccione...</option>');
+        $("#location-com-map").prop("disabled",true);
+        showBoroughs(); // with 0 there is no boroughs
+    }
+});
+
+/*Padding for the navigation bar*/
+$("#navigationMenu").resize(function(){
+    $("navBarSpacing").height($("#navigationMenu").height()+10);
+});
